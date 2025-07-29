fbf639b021bbb7160b4fb7014ca836695e42e831
Bhavan Kumar <166432695+bhavankumar2004@users.noreply.github.com>
2025-07-29 10:33:34 +0530

    Delete Employee dashboard directory

--- Employee dashboard/config.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',         # Default MySQL username
-    'password': 'Windows@11',  # Your MySQL password
-    'database': 'employee_performance'
-}
